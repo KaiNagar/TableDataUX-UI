34cce7d4ba7e9c5ac8b4e4c0661287a52ea347c6
Kai Nagar <kaynagar@gmail.com>
2023-06-14 19:16:17 +0300

    creating main route for the table data fetures

---
+++ src/pages/TableIndex.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import { useEffectUpdate } from "../customHooks/useEffectUpdate"
+import { dataService } from "../services/data.service"
+import { TableData } from "./TableData"
+
+export const TableIndex = () => {
+  const [data, setData] = useState()
+
+  useEffectUpdate(() => {
+    loadData()
+  }, [])
+
+  const loadData = async () => {
+    const dataToUse = await dataService.query()
+    setData(dataToUse)
+  }
+  return (
+    <section className='table-index'>
+      
+        {data ? <TableData data={data} /> : <span>Loading Data...</span>}
+    </section>
+  )
+}
