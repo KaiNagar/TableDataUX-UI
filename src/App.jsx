3bc7a90e00e4b04689a0330192aff0c29c4f16fe
Kai Nagar <kaynagar@gmail.com>
2023-06-14 19:15:08 +0300

    setting main routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,22 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import './assets/styles/style.scss'
 
 import { Home } from './pages/Home'
-import { Login } from './pages/Login'
 
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
-import { useState } from 'react'
-import { dataService } from './services/data.service'
-import { TableData } from './pages/TableData'
-import { useEffectUpdate } from './customHooks/useEffectUpdate'
+import { TableIndex } from './pages/TableIndex'
 
 export function App() {
-  const [data, setData] = useState()
-
-  useEffectUpdate(() => {
-    loadData()
-  }, [])
-
-  const loadData = async () => {
-    const dataToUse = await dataService.query()
-    setData(dataToUse)
-  }
-
   return (
     <Router>
       <div className='App'>
         <AppHeader />
         <div className='main-container'>
           <div className='main layout'>
-            {data ? <TableData data={data} />: <span>Loading Data...</span> }
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/table' element={<TableIndex />} />
+            </Routes>
           </div>
         </div>
         <AppFooter />
