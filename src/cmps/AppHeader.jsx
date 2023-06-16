6e9b851f46780ff1d14582bb44e035c401388927
Kai Nagar <kaynagar@gmail.com>
2023-06-16 22:44:20 +0300

    imported logo into header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from 'react-router-dom'
+import logo from '../assets/imgs/First Assigment1logo.png'
 
 export const AppHeader = () => {
   return (
     <section className='app-header-constainer '>
       <div className='app-header layout flex align-center space-between'>
-        <div className='logo'>logo</div>
+        <div className='logo'><img src={logo} alt="" /></div>
         <div className='navigation'>
           <NavLink className='header-link' to='/'>
             Home
