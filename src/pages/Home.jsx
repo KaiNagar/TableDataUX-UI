62e0c3e71430c675df2546cea11a444f3dce91a4
Kai Nagar <kaynagar@gmail.com>
2023-06-17 13:06:22 +0300

    home page added text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
+import { NavLink } from "react-router-dom"
+
 export const Home = () => {
     return (
-        <section className="home-page">Home</section>
+        <section className="home-page">
+            <h1 className="home-header">Client Side - Assigment 1</h1>
+            <h2>Hope you will enjoy my work</h2>
+            <div className="action">
+                <NavLink className='home-link' to={'/table'}>To move to the table click me!</NavLink>
+                <h3>Or on 'Table' in the header</h3>
+            </div>
+        </section>
     )
 }
