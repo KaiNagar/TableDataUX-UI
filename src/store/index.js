d377c3dca8c3615357df2f6c3998ef979b6d8df2
Kai Nagar <kaynagar@gmail.com>
2023-06-24 19:46:41 +0300

    removed thunk from store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,7 @@
-import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, combineReducers } from 'redux';
 import { dataReducer } from './reducers/dataReducer';
 
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   dataModule: dataReducer,
@@ -11,5 +9,4 @@ const rootReducer = combineReducers({
 
 export const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
 );
