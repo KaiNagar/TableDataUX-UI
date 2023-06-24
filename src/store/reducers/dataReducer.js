144556b9bb05eb89975a19bb0e69d772f7ae76e8
Kai Nagar <kaynagar@gmail.com>
2023-06-24 19:46:35 +0300

    cleaned from useless variables

--- src/store/reducers/dataReducer.js
+++ src/store/reducers/dataReducer.js
@@ -20,11 +20,6 @@ export const UNDO_UPDATE_CELL = 'UNDO_UPDATE_CELL'
 
 export function dataReducer(state = INITIAL_STATE, action) {
     let data
-    let lastRemovedRow
-    let lastRemovedColumn
-    let newColumns
-    let newRows
-    let lastUpdatedCellValue
     switch (action.type) {
         case LOAD_DATA:
             return { ...state, data: action.data }
@@ -35,15 +30,15 @@ export function dataReducer(state = INITIAL_STATE, action) {
             data = { ...state.data, rows: [...state.data.rows, action.row] }
             return { ...state, data }
         case REMOVE_COLUMN:
-            lastRemovedColumn = state.data.columns.find(c => c.id === action.columnId)
-            newColumns = state.data.columns.filter(c => c.id !== action.columnId)
+            let lastRemovedColumn = state.data.columns.find(c => c.id === action.columnId)
+            let newColumns = state.data.columns.filter(c => c.id !== action.columnId)
             return { ...state, data: { ...state.data, columns: newColumns } }
         case REMOVE_ROW:
-            lastRemovedRow = state.data.rows.find(r => r.id === action.rowId)
-            newRows = state.data.rows.filter(r => r.id !== action.rowId)
+            let lastRemovedRow = state.data.rows.find(r => r.id === action.rowId)
+            let newRows = state.data.rows.filter(r => r.id !== action.rowId)
             return { ...state, data: { ...state.data, rows: newRows } }
         case UPDATE_CELL:
-            lastUpdatedCellValue = state.data.rows[action.info.rowIdx][action.info.columnId]
+            let lastUpdatedCellValue = state.data.rows[action.info.rowIdx][action.info.columnId]
             data = { ...state.data, rows: state.data.rows.map((r, i) => i === action.info.rowIdx ? { ...r, [action.info.columnId]: action.info.value } : r) }
             return { ...state, data }
         case SET_FILTER:
